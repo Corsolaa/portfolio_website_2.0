93dd9da02d83e142a3486a3a3b2299d02328ffe8
John <bruno.bouwman4@gmail.com>
2023-06-06 15:12:53 +0200

    Transformed PHP
    Converted everything to PHP.

---
+++ template_parts/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+    <div class="left">
+        <img class="duck" src="/images/gold_duck.ico" alt="">
+        <h2 class="name">Bruno</h2>
+    </div>
+    <div class="right">
+        <a class="active header-item" href="#">Home</a>
+        <a class="header-item" href="#projects">Projecten</a>
+        <a class="header-item" href="#contact">Contact</a>
+    </div>
+</div>
