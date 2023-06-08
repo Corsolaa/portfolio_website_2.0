d750fb645edea952f55c1bc0e0df5e2b300dd174
John <bruno.bouwman4@gmail.com>
2023-06-08 16:54:02 +0200

    moved head tag to other php file.
    Moved <head> part to other php file.

---
+++ template_parts/head.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="description of the website.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex, nofollow"/>
+    <meta name="author" content="Bruno B">
+
+    <title>Bruno B</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/gold_duck.ico">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet">
+</head>
