692a82da08e7977baa52321ab900ee02ebfb9646
John <bruno.bouwman4@gmail.com>
2023-06-12 10:59:06 +0200

    Added another company to the list
    Added VillaArena project to the mix.
    Changed some stuff from Erkend.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include("template_parts/head.php");
 <?php include("template_parts/header.php") ?>
 
 <div class="content">
-    <div class="section module_6-4">
+    <div class="section smoll_sec module_6-4">
         <div class="container info_box info_box_1">
             <div class="top-section">
                 <h1>Hello, I'm Bruno. <br>
@@ -38,7 +38,7 @@ include("template_parts/head.php");
         <div class="container pic_cover picture_box_1"></div>
     </div>
 
-    <div class="section module_4-6">
+    <div class="section smoll_sec module_4-6">
         <div class="container pic_cover picture_box_2"></div>
         <div class="container info_box info_box_2">
             <div class="technologies">
@@ -58,16 +58,25 @@ include("template_parts/head.php");
     <div class="section module_1-1">
         <div class="container project">
             <div class="inside">
-                <div class="image">
-                    <img src="images/logo-epv.png" alt="erkend_verhuizen_logo">
-                </div>
                 <div class="text_space">
-                    <h1>Erkend Verhuizen</h1>
+                    <h2>Erkend Verhuizen</h2>
+                    <p>Dashboard to find recognised movers</p>
+                </div>
+                <div class="image">
+                    <img src="images/erkend_verhuizen.png" alt="erkend_verhuizen_logo">
                 </div>
             </div>
         </div>
-        <div class="container" style="background: red;">
-
+        <div class="container project" style="background: gray;">
+            <div class="inside">
+                <div class="text_space">
+                    <h2>VillaArena</h2>
+                    <p>A quiz program that gives your personal</p>
+                </div>
+                <div class="image">
+                    <img src="images/villaarena_woonquiz.png" alt="villa-arena_woon-quiz">
+                </div>
+            </div>
         </div>
     </div>
 </div>
