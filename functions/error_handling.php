a226913e3a4095e82bb92912893c107dedaf629b
Corsolaa🍎 <bruno.bouwman4@gmail.com>
2023-07-04 15:56:08 +0200

    Added more errors.
    Added email validation.
    Added die after redirect.

--- functions/error_handling.php
+++ functions/error_handling.php
@@ -1,8 +1,13 @@
 <?php
-function error_handling(string $error) {
+function error_handling(string $error): string
+{
     switch ($error) {
         case "not_filled":
             return "Please fill in all fields.";
+        case "invalid_mail":
+            return "Please fill in a valid email.";
+        case "invalid_name":
+            return "Please fill in a valid name.";
     }
     return "An error has occurred";
 }
--- functions/set_contact.php
+++ functions/set_contact.php
@@ -13,10 +13,18 @@ include "../functions/clean_data.php"
 </pre>
 
 <?php
-
 if (empty($_POST["email"]) || empty($_POST["name"]) || empty($_POST["message"])) {
     header("Location: /contact?error=not_filled");
+    die();
 }
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    header("Location: /contact?error=invalid_mail");
+    die();
+}
+
+$email = cleanData($_POST["email"]);
+$name = cleanData($_POST["name"]);
+$message = cleanData($_POST["message"]);
 
 //INSERT INTO `email_requests` (`id`, `email`, `name`, `message`, `requested_on`)
 // VALUES (NULL, 'bruno@oversticht.eu', 'Naam', 'This is a text message.', current_timestamp());
